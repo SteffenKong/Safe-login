947e0dcb0a34291b211f4e397bf5f9eb7d296efe
SteffenKong <3266023724@qq.com>
2019-09-11 17:21:08 +0800

    feat:增加秘钥配置

---
+++ config/secret.php
@@ -0,0 +1,34 @@
+<?php
+//项目的秘钥管理
+return [
+    'rsa'=>[
+        'publicKey'=>'-----BEGIN PUBLIC KEY-----
+                MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDB+8uX3RO7ybG18/WfHQw4W6vD
+                2IuWy8KT7NAyZMD6e6G8y3tBe8Vp6UZ6UOVkBi6DQdt9b8Ng/dhhxBMnwVxZa5im
+                TKiOpXMYrVYxS73MXd1Rhlh0TY5seHZZwnQd0FdRqNvuBgsC9v1NHVshygc8DgkO
+                y0f3WSAKu6mxH/tYfQIDAQAB
+        -----END PUBLIC KEY-----',
+
+        'privateKey'=>'-----BEGIN PRIVATE KEY-----
+            MIICdQIBADANBgkqhkiG9w0BAQEFAASCAl8wggJbAgEAAoGBAMH7y5fdE7vJsbXz
+            9Z8dDDhbq8PYi5bLwpPs0DJkwPp7obzLe0F7xWnpRnpQ5WQGLoNB231vw2D92GHE
+            EyfBXFlrmKZMqI6lcxitVjFLvcxd3VGGWHRNjmx4dlnCdB3QV1Go2+4GCwL2/U0d
+            WyHKBzwOCQ7LR/dZIAq7qbEf+1h9AgMBAAECgYASj8RweKANxQPzNC/ZlM75iZQK
+            /mqE+GRbATr+x3NLxI66YQIfRveh5cdvptP8WTcUKjYVb59TZZbR7BAlKrRqU1VO
+            ced2ivXtIdaecR30XXxc3zysfGqBBuEfZLQ3MqUXAq96HfJqjxF89avdsxUUpti6
+            Ws4WlFY4e1vJPqJtBQJBAOoqBvWL8fBkw3zCNNDM2i2X3yLywlTdl/jLyTpYidG1
+            4a1Kyp/rtRAIitIs4T9UGa5U/PrQ0YbTivnADS4bf/8CQQDUEpNzxe1Qp0rKH3do
+            mHaZkIFi93wfWbIxHfWoK2vfGogzJdK3Nyo+enPd0ss4FVL0u2jTrB695UP4HUwo
+            lyeDAkAtJ9mbtFb9gpDoGBNTSI2ySR0wghvTfb/2rGWrqLM7C5XvcwLMAAo0g1Ex
+            4pG1QqSJJ755J5egJl8gdlaUU51FAkB4IV2tgzz/JwTSoih983+bVaQ1b4O8Lo8m
+            C5aQgKhGZT0p3A8VAgbIBY2E5ld/mXS4dUDYRvPTFf/+mHn/tEAJAkBPLsbeLU2W
+            DdLGm8KQt4hSWRZwfgYbYxq8+BniVNxMPGFLWqBtE3zzWkDUBq8BM/xMIJj6Ax6z
+            Z3MZ0z8e3+DS
+            -----END PRIVATE KEY-----
+            '
+    ],
+
+    'jwt'=>[
+
+    ]
+];
